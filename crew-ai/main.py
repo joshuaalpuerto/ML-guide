5093724f528bc67809aa0c9f90754654f023fa65
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2024-01-09 11:20:46 +0200

    Log the AI process to understand it better

--- crew-ai/main.py
+++ crew-ai/main.py
@@ -3,16 +3,19 @@ import config
 from crewai import Agent, Task, Crew, Process
 from langchain.tools import DuckDuckGoSearchRun
 from langchain_community.chat_models.fireworks import ChatFireworks
+from langchain.globals import set_llm_cache, set_debug
+from langchain.cache import InMemoryCache
 
-os.environ["OPENAI_API_KEY"] = "Your Key"
-
+set_llm_cache(InMemoryCache())
+# Turn this on only if you want to debug other wise it's hard to see the conversations.
+set_debug(True)
 
 search_tool = DuckDuckGoSearchRun()
 
 llm = ChatFireworks(
     model="accounts/fireworks/models/mistral-7b-instruct-4k",
     fireworks_api_key=config.FIREWORKS_API_KEY,
-    model_kwargs={"temperature": 0.1, "max_tokens": 1024, "top_p": 0.9},
+    model_kwargs={"temperature": 0.5, "max_tokens": 4096},
 )
 
 # Define your agents with roles and goals
@@ -67,7 +70,4 @@ if __name__ == "__main__":
     )
 
     # Get your crew to work!
-    result = crew.kickoff()
-
-    print("######################")
-    print(result)
+    crew.kickoff()
