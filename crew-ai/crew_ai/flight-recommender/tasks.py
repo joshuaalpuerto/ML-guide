b78d47ade2f064706ca249ac09de4883fe528e54
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2024-01-13 15:39:21 +0200

    Format tool response

--- crew-ai/crew_ai/flight-recommender/tasks.py
+++ crew-ai/crew_ai/flight-recommender/tasks.py
@@ -4,18 +4,18 @@ from datetime import date
 
 
 class Tasks:
-    def research(self, agent):
+    def research(self, query, agent):
         return Task(
             description=dedent(
-                f"""Analyze the user query about its trip and conduct a comprehensive research to get cheap flights."""
+                f"""Analyze the user query about its trip and conduct a comprehensive research to get cheap flights. Query: {query}"""
             ),
             agent=agent,
         )
 
-    def suggest(self, agent, origin, interests, range):
+    def suggest(self, agent):
         return Task(
             description=dedent(
-                f"""Using the insights from the researcher's report, provide a comprehensive summary of what is the cheapest flight for the user.
+                f"""Using the insights from the researcher's report, provide a comprehensive summary of what is the cheapest flight for the user. Suggest the best iterenary that requires fastest travel time, less layovers and date align with user travel date.
       """
             ),
             agent=agent,
--- crew-ai/crew_ai/flight-recommender/tools/amadeus_tool.py
+++ crew-ai/crew_ai/flight-recommender/tools/amadeus_tool.py
@@ -6,6 +6,8 @@ from amadeus import Client, ResponseError
 from pydantic import BaseModel, Field
 from langchain.agents import tool
 from langchain.tools import BaseTool
+import airportsdata
+from textwrap import dedent
 
 
 # Set up logging
@@ -56,7 +58,7 @@ class GetAmadeusFlightOffers(BaseModel):
 
 class GetFlightOffers(BaseTool):
     name = "get_flight_offers"
-    description = "Pulls the flight data from the amadeus server and saves it to the neon database."
+    description = "Search for flight information."
     args_schema: Type[BaseModel] = GetAmadeusFlightOffers
     return_direct = True
 
@@ -81,7 +83,7 @@ class GetFlightOffers(BaseTool):
         Pulls the flight data from the amadeus server.
         """
 
-        # Initialize Amadeus client
+        # # Initialize Amadeus client
         amadeus = Client(
             client_id=config.AMADEUS_API_KEY, client_secret=config.AMADEUS_SECRET
         )
@@ -112,10 +114,12 @@ class GetFlightOffers(BaseTool):
 
         try:
             response = amadeus.shopping.flight_offers_search.get(**params)
-            logger.debug("Response data: %s", response.data)
+            # logger.debug("Response data: %s", response.data)
             # we need to filter the response because it's alot, let's focus on what we care about.
             # price, number of stops, etc.
-            return json.dumps(response.data)
+            result = [self.format_item_result(item) for item in response.data]
+
+            return "\n\n".join(result)
         except ResponseError as error:
             logger.error(f"An error occurred: {error}")
             logger.error(f"Error code: {error.code}")
@@ -126,3 +130,80 @@ class GetFlightOffers(BaseTool):
                 )  # Log full error response
             return ""
 
+    def format_item_result(self, data):
+        itineraries = data["itineraries"]
+
+        itineraries_summary = dedent(f"""Flight offer {data['id']}""")
+        for idx, iterenary in enumerate(itineraries):
+            layovers = self.get_layovers_info_from_iterenary(iterenary)
+
+            options = dedent(
+                f"""
+                Itinerary {idx + 1}:
+                Layover(s): {", ".join(layovers)}
+                """
+            )
+
+            for segment in iterenary["segments"]:
+                departure_details = segment["departure"]
+                arrival_details = segment["arrival"]
+
+                departure_airport_details = self.get_iata_info(
+                    departure_details["iataCode"]
+                )
+                arrival_airport_details = self.get_iata_info(
+                    arrival_details["iataCode"]
+                )
+
+                flight_details = dedent(
+                    f"""
+                Departure: {departure_airport_details['name']}
+                Terminal: {departure_details.get('terminal', '')}
+                Date: {departure_details['at']}
+
+                Arrival: {arrival_airport_details['name']}
+                Terminal: {arrival_details.get('terminal', '')}
+                Date: {arrival_details['at']}
+                """
+                )
+
+                options += flight_details + "\n"
+
+        return itineraries_summary + options
+
+    def get_layovers_info_from_iterenary(self, iterenary):
+        segments = iterenary["segments"]
+        layovers = []
+
+        for segment in segments[1:]:
+            airport_info = self.get_iata_info(segment["departure"]["iataCode"])
+            layovers.append(airport_info["name"])
+
+        return layovers
+
+    def get_iata_info(self, iataCode):
+        airports = airportsdata.load("IATA")
+        return airports[iataCode]
+
+
+if __name__ == "__main__":
+    search_flight_offers_with_amadeus_client_tool = GetFlightOffers()
+    result = search_flight_offers_with_amadeus_client_tool.run(
+        {
+            "originLocationCode": "BOS",
+            "destinationLocationCode": "PAR",
+            "departureDate": "2024-01-30",
+            "returnDate": "2024-02-14",
+            "adults": 1,
+            "returnDate": None,
+            "children": None,
+            "infants": None,
+            "travelClass": None,
+            "includedAirlineCodes": None,
+            "excludedAirlineCodes": None,
+            "nonStop": "false",
+            "currencyCode": "EUR",
+            "maxPrice": None,
+            "max": 5,
+        }
+    )
