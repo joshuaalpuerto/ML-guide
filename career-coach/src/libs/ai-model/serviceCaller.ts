1b50fd791e50543c5db20cbcbeb82ef97fde52fd
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2025-07-05 22:15:06 +0300

    Add service caller

---
+++ career-coach/src/libs/ai-model/serviceCaller.ts
@@ -0,0 +1,26 @@
+import { generateText, type LanguageModel } from 'ai';
+import { createOpenAI } from '@ai-sdk/openai';
+
+type LLMClientParams = {
+  model: LanguageModel;
+}
+
+export async function createLLMClient(config: LLMClientParams) {
+  let model = config.model;
+
+  if (!model) {
+    const openai = createOpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    })
+    model = openai('gpt-4o-mini')
+  }
+
+  return {
+    generateText: async (prompt: string) => {
+      return generateText({
+        model: model,
+        prompt
+      });
+    }
+  }
+}
